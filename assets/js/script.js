a314ade55b85ff8c48e681cc05ed7d18dc112ac2
MelanieS <melanie.salinas@mail.udp.cl>
2021-05-23 00:27:32 -0400

    first commit viajeschile

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+$(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+
+    $('#cambiarectangulo').dblclick(function(){
+        $(this).css('background-color', 'red');
+        });
+
+        $('#cambiarectangulo2').dblclick(function(){
+            $(this).css('background-color', 'red');
+            });
+
+            $('#cambiarectangulo3').dblclick(function(){
+                $(this).css('background-color', 'red');
+                });
+    });
+    
+	$(".titulocard1").click(function () {
+        $('.cuerpocard1').toggle(['slow'] ,['show']);
+    });
+    $(".titulocard2").click(function () {
+        $('.cuerpocard2').toggle(['slow'] ,['show']);
+
+    });
+    $(".titulocard3").click(function () {
+        $('.cuerpocard3').toggle(['slow'] ,['show']);
+    });
+    $(".titulocard4").click(function () {
+        $('.cuerpocard4').toggle(['slow'] ,['show']);
+
+    });
+
